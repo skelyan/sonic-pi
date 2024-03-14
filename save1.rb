2323648abf3b593cf3d2baa8af595408da4772ea
skelyan <153768209+skelyan@users.noreply.github.com>
2024-03-13 17:31:43 -0700

    Add files via upload

---
+++ save1.rb
@@ -0,0 +1,65 @@
+#measure 1
+sleep 2
+sleep 1.5
+sleep 0.25
+sleep 0.125
+play :b4
+sleep 0.125
+play :c5
+sleep 0.25
+play :c5
+sleep 0.125
+
+#measure 2
+play :d5
+sleep 1.5
+play :d5
+sleep 0.25
+play :d5
+sleep 0.125
+play :d5
+sleep 0.25
+play :c5
+sleep 0.25
+play :c5
+sleep 1
+play :f5
+sleep 1
+
+#measure 3
+play :c5
+sleep 1
+play :c5
+sleep 1.5
+play :c5
+sleep 0.125
+play :d5
+sleep 0.125
+play :c5
+sleep 0.125
+play :b4
+sleep 1
+play :b4
+sleep 0.5
+play :c4
+sleep 0.25
+play :c4
+sleep 0.25
+play :a4
+sleep 0.25
+
+#measure 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
